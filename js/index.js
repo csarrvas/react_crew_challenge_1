3723402937f410e51155d1acf6c9909fc1b4a018
Cesar Rivas <crivas@applaudostudios.com>
2021-02-09 14:01:19 -0600

    ✨ Slider and Add button functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const formToAdd = document.getElementsByClassName('shop__add-to-cart-form')[0];
+const productAdded = document.getElementsByClassName('shop__product-added')[0];
+const imgSelector = document.getElementById('img-selector');
+const imgActive = document.getElementById('img-active');
+
+formToAdd.addEventListener('submit', (e) => {
+  e.preventDefault();
+  productAdded.style.display = 'block';
+  setTimeout(() => {
+    productAdded.style.display = 'none';
+  }, 3000);
+});
+
+imgSelector.addEventListener('click', (e) => {
+  if (e.target.tagName === 'FIGURE') {
+    let selector = imgSelector.firstElementChild;
+    while (selector) {
+      selector.classList.remove('active');
+      selector = selector.nextElementSibling;
+    }
+    e.target.classList.add('active');
+    imgActive.firstElementChild.src = e.target.firstElementChild.src;
+  }
+});
